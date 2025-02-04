96ecc612e4c45316ab703137fc75fbddcb45672e
Mike <mgonzalezobenza@gmail.com>
2025-02-04 12:55:13 +0100

    typo fix

--- src/components/content/aboutme.tsx
+++ src/components/content/aboutme.tsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
     return (
     <div className="content">
         <h1 className="title">About Me</h1>
-        <p>I am Miguel "Mike" González Obenza, or Mike Obenza for short. I'm a developer located in Spain interested in may different topics. I've studied a degree on <em>Audiovisual Communication</em> and later a master's degree on <em>Design, Development, and Marketing of Video Games</em>.</p>
+        <p>I am Miguel "Mike" González Obenza, or Mike Obenza for short. I'm a developer located in Spain interested in many different topics. I've studied a degree on <em>Audiovisual Communication</em> and later a master's degree on <em>Design, Development, and Marketing of Video Games</em>.</p>
         <p>On my spare time I'm constantly learning different disciplines, like 3D art, frontend development, game design, shader programing, rendering pipeline, music composition and sound design.</p>
         <div className="button-div" onClick={HandleOnClick}><button className="contact-button">Contact Me!</button></div>
     </div>
