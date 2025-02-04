016609b9d70e09a4a2faa8538bcda1b816bee2e4
Mike <mgonzalezobenza@gmail.com>
2025-02-04 12:23:17 +0100

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mikego2611.github.io"
+  base: "/mike-web-porfolio/"
 })
